32982dca7ac170eadb00360295f39eca09e6fb2c
alokgudikote <alok.gudikote@byjus.com>
2022-04-09 21:53:59 +0530

    refactor : moshi adapter addition

--- network/src/main/java/com/alokomkar/network/di/NetworkModule.kt
+++ network/src/main/java/com/alokomkar/network/di/NetworkModule.kt
@@ -7,6 +7,8 @@ import com.alokomkar.network.api.UserStocksAPI
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.chuckerteam.chucker.api.RetentionManager
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +16,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -33,12 +35,17 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit
             = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .client(okHttpClient)
         .build()
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi
+        = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     @Singleton
     @Provides
